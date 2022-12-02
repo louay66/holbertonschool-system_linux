a63c4ec3985fbb5bd9f6634b53439e8b4cdf2d12
louay66 <louaylouay20@gmail.com>
2022-12-02 14:46:35 +0100

    main.h

--- 0x00-ls/main.h
+++ 0x00-ls/main.h
@@ -14,6 +14,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int ls_dir(char *buff, int bool);
 
 #endif
