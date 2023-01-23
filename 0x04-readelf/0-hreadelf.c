dcb6f411577c1dd4c4d7efc0e06d122be9507ef8
louay66 <louaylouay20@gmail.com>
2023-01-23 16:18:44 +0100

    first task

--- 0x04-readelf/0-hreadelf.c
+++ 0x04-readelf/0-hreadelf.c
@@ -9,23 +9,7 @@
  * @env:  the environment
  * Return: -1 in fail and 0 in success
  */
-int main(int argc, char **argv, char *env[])
+int main(void)
 {
-	char *readELF = "/usr/bin/readelf";
-	char *args[4];
-
-	if (argc != 2)
-		exit(0);
-
-	args[0] = readELF;
-	args[1] = "-W";
-	args[2] = "-h";
-	args[3] = argv[1];
-	args[4] = NULL;
-	if (execve(readELF, args, env) == -1)
-	{
-		perror("Error: ");
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
+   return (EXIT_SUCCESS);
 }
