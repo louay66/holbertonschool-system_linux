5894210e52893caf0b60ae47e3f8e0ab466a6a54
louay66 <louaylouay20@gmail.com>
2023-02-06 10:53:14 +0100

    all good

---
+++ 0x05-libasm/2-strncmp.asm
@@ -0,0 +1,43 @@
+BITS 64
+
+global asm_strncmp
+
+section .text
+   asm_strncmp:
+         push rbp
+         mov rbp, rsp
+         xor rcx, rcx
+         xor rax, rax
+   loop_in_bytes_of_sour_and_dest:
+            cmp rcx, rdx
+            je compar
+            mov al, [rdi + rcx]
+            mov bl, [rsi + rcx]
+            cmp al, 0
+            je compar
+            cmp bl, 0
+            je compar
+            cmp al , bl
+            jne compar
+            inc rcx
+            jmp loop_in_bytes_of_sour_and_dest
+   compar:
+      cmp al , bl
+      je equal
+      jg greater
+      jl less
+   equal:
+      mov rax , 0
+      jmp return
+   greater:
+      sub al, bl
+      jmp return
+   less:
+      sub bl, al
+      mov al , bl
+      neg rax
+      jmp return
+   return:
+      mov rsp , rbp
+      pop rbp
+      ret
