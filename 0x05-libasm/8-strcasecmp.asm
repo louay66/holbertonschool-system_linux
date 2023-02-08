043580743a8fc6adbee9399b8a88fa0c39cda20a
louay66 <louaylouay20@gmail.com>
2023-02-08 09:09:47 +0100

    strcasecmp

--- 0x05-libasm/8-strcasecmp.asm
+++ 0x05-libasm/8-strcasecmp.asm
@@ -9,43 +9,41 @@ section .text
          xor rcx, rcx
          xor rax, rax
          xor rdx, rdx
-         xor rbx, rbx
+         xor r11, r11
 
    loop_in_bytes_of_sour_and_dest:
-            mov dl, [rdi + rcx]
-            mov bl, [rsi + rcx]
-            mov r8b, [rdi]
-            mov r9b, [rsi]
-            cmp r8b, 0
-            je result_zero
-            cmp r9b, 0
-            je result_zero
-            cmp dl, 65
-            jl recheck
-            cmp dl, 90
-            jg recheck
-            add dl , 32
+         mov dl, [rdi + rcx]
+         mov r11b, [rsi + rcx]
+         cmp dl, 0
+         je NULL_bytes
+         cmp r11b, 0
+         je NULL_bytes
+         cmp dl, 65
+         jl recheck
+         cmp dl, 90
+         jg recheck
+         add dl, 32
    recheck:
-            cmp bl, 65
-            jl result
-            cmp bl, 90
-            jg result
-            add bl, 32
+         cmp r11b, 65
+         jl result
+         cmp r11b, 90
+         jg result
+         add r11b, 32
    result:
-      mov eax, edx
-      sub eax, ebx
-      cmp eax, 0
-      jne return
-      cmp edx, 0
-      je return
-      inc rcx
-      jmp loop_in_bytes_of_sour_and_dest
-   result_zero:
-      mov eax, edx
-      sub eax, ebx
-      jmp return
+         mov eax, edx
+         sub eax, r11d
+         cmp eax, 0
+         jne return
+         cmp dl, 0
+         je return
+         inc rcx
+         jmp loop_in_bytes_of_sour_and_dest
+   NULL_bytes:
+         mov eax, edx
+         sub eax, r11d
+         jmp return
 
    return:
-   mov rsp , rbp
-   pop rbp
-   ret
+         mov rsp , rbp
+         pop rbp
+         ret
