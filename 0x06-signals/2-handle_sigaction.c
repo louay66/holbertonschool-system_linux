c1bbde2a138b1f2c378ca3b61d5b176b08cd042e
louay66 <louaylouay20@gmail.com>
2023-02-14 12:15:32 +0100

    2-handle_sigaction.c

---
+++ 0x06-signals/2-handle_sigaction.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include "signals.h"
+
+/**
+ * handler - msg handler
+ *@num: number of signum of signal
+ */
+void handler(int num)
+{
+	printf("Gotcha! [%d]\n", num);
+}
+
+/**
+ * handle_sigaction - catch the interprtur ctr-c
+ * Return: 0 if success or -1 if failure
+ */
+int handle_sigaction(void)
+{
+	struct sigaction action;
+
+	action.sa_handler = handler;
+
+	return (sigaction(SIGINT, &action, NULL));
+}
