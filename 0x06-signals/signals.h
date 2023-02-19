aa9dc95dc2966ad15f3502cae3388690c37dd16d
louay66 <louaylouay20@gmail.com>
2023-02-19 21:57:08 +0100

    signals.h

---
+++ 0x06-signals/signals.h
@@ -0,0 +1,9 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+
+#endif
